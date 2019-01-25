3241e77e5b39aad21cf64215bb284bd1a2bb1779
stackprobe <stackprobes@gmail.com>
2019-01-26 00:45:22 +0900

    coffee break

--- WHTTR/WHTTR/BootTools.cs
+++ WHTTR/WHTTR/BootTools.cs
@@ -6,6 +6,8 @@ using System.IO;
 
 namespace WHTTR
 {
+	// sync > @ BootTools
+
 	public class BootTools
 	{
 		public static string SelfFile;
@@ -19,4 +21,6 @@ namespace WHTTR
 			Directory.SetCurrentDirectory(SelfDir);
 		}
 	}
+
+	// < sync
 }
